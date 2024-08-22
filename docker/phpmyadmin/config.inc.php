37380716625ba4fadd69cd54564fa53941ea0a13
Flatline <belthazor.hd@gmail.com>
2024-08-22 22:21:20 +1000

    Start

---
+++ docker/phpmyadmin/config.inc.php
@@ -0,0 +1,7 @@
+<?php
+$cfg['Servers'][1]['auth_type'] = 'cookie';  // Включает cookie-based аутентификацию
+$cfg['Servers'][1]['host'] = 'db';  // Имя хоста MySQL (укажите то, что используется в `PMA_HOST`)
+$cfg['Servers'][1]['port'] = '3306';  // Порт MySQL
+$cfg['Servers'][1]['user'] = 'root';  // Пользователь по умолчанию (root)
+$cfg['Servers'][1]['password'] = 'root3322';  // Пароль пользователя root
+$cfg['Servers'][1]['AllowNoPassword'] = false;  // Запрещает доступ без пароля
